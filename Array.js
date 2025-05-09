6538d2675629f3af335f86c404d2a4e79ae1c201
Jyoti Sharma <130760557+jyotisharma705@users.noreply.github.com>
2025-05-09 18:21:01 +0530

    Add files via upload
    How to declare Array. Methods of Array .

---
+++ Array.js
@@ -0,0 +1,46 @@
+//Arrays in JS :
+
+//Collections of items.
+
+//Create Array
+
+let heroes = ["ironman", "hulk", "thor", "batman"];
+
+let marks = [96, 75, 48, 83, 66];
+
+let info = ["rahul", 86, "Delhi"];
+
+console.log(marks);
+console.log(marks.length);
+
+// Array Indices
+
+arr[0], arr[1], arr[2]
+
+let arr = [12, 15, 55, 89, 63];
+
+console.log(arr[0]);
+
+// to change value of array
+
+/* Looping over an Array*/
+
+// Print all elements of an array
+
+
+// Array Methods
+
+Push() : Add to end
+Pop() : Delete from end & return
+toString() : Converts array to String
+Concat(): joins multiple arrays & returns result
+Unshift() : add to start
+shift() : delete from start & return
+slice() : returns a piece of the array
+
+slice(startIdx, endIdx)
+
+splice() : change original array(add, remove, replace)
+splice(startIdx, delCount, newEl1...)
+
+
